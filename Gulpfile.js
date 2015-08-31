422e6316f411444908389396a95d8de8a6ecb0fd
illus0r <zor667@gmail.com>
2015-08-31 13:07:54 +0300

    gulp gulps! hooray

--- Gulpfile.js
+++ Gulpfile.js
@@ -26,59 +26,90 @@
 
 
 
-//var gulp        = require('gulp');
-//var sass        = require('gulp-sass');
-//var neat = require('node-neat').includePaths;
-//var browserSync = require('browser-sync').create();
-
-//// Static Server + watching scss/html files
-//gulp.task('serve', ['sass'], function() {
-
-    //browserSync.init({
-        //server: ""
-    //});
-
-    //gulp.watch("scss/*.scss", ['sass']);
-    //gulp.watch("*.html").on('change', browserSync.reload);
-//});
-
-//// Compile sass into CSS & auto-inject into browsers
-//gulp.task('sass', function() {
-    //return gulp.src("scss/*.scss")
-        //.pipe(sass({
-          //includePaths: ['styles'].concat(neat)
-        //}))
-        //.pipe(gulp.dest("css"))
-        //.pipe(browserSync.stream());
-//});
-
-//gulp.task('default', ['serve']);
-
-
-
-
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
 var neat = require('node-neat').includePaths;
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function () {
-    gulp.src('scss/styles.scss')
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: ""
+    });
+
+    gulp.watch(["scss/*.scss", "scss/components/*.scss"], ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
         .pipe(sass({
           includePaths: ['styles'].concat(neat)
         }))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init(["css/*.css", "js/*.js", "*.html"], {
-        server: {
-            baseDir: "./"
-        }
-    });
-});
+gulp.task('default', ['serve']);
 
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-});
+
+
+
+
+//var gulp = require('gulp');
+//var sass = require('gulp-sass');
+//var neat = require('node-neat').includePaths;
+//var browserSync = require('browser-sync');
+
+//gulp.task('sass', function () {
+    //gulp.src('scss/styles.scss')
+        //.pipe(sass({
+          //includePaths: ['styles'].concat(neat)
+        //}))
+        //.pipe(gulp.dest('css'));
+//});
+
+//gulp.task('browser-sync', function() {
+    //browserSync.init(["css/*.css", "js/*.js", "*.html"], {
+        //server: {
+            //baseDir: "./"
+        //}
+    //});
+//});
+
+//gulp.task('default', ['sass', 'browser-sync'], function () {
+    //gulp.watch(["scss/*.scss", "scss/components/*.scss"], ['sass']);
+//});
+
+
+
+//var gulp = require('gulp');
+//var sass = require('gulp-sass');
+//var browserSync = require('browser-sync');
+//var neat = require('node-neat').includePaths;
+
+//var paths = {
+  //scss: './scss/*.scss'
+//};
+
+//gulp.task('sass', function () {
+    //gulp.src('scss/styles.scss')
+        //.pipe(sass({
+          //includePaths: ['scss']
+        //}))
+        //.pipe(gulp.dest('css'));
+//});
+
+//gulp.task('browser-sync', function() {
+    //browserSync.init(["css/*.css", "js/*.js", "*.html"], {
+        //server: {
+            //baseDir: "./"
+        //}
+    //});
+//});
+
+//gulp.task('default', ['sass', 'browser-sync'], function () {
+    //gulp.watch("scss/*.scss", ['sass']);
+//});
